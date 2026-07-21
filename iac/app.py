494bed30065345967ffa918ff1a5a564f7e87287
Guilherme Britto <guibrittotava@gmail.com>
2026-07-21 13:03:56 -0300

    update: update app.py file

--- iac/app.py
+++ iac/app.py
@@ -4,7 +4,7 @@ import os
 import aws_cdk as cdk
 from adjust_layer_directory import adjust_layer_directory
 
-from iac.template_stack import TemplateStack
+from stack.iac_stack import IacStack
 
 
 
@@ -20,27 +20,21 @@ app = cdk.App()
 aws_region = os.environ.get("AWS_REGION")
 aws_account_id = os.environ.get("AWS_ACCOUNT_ID")
 stack_name = os.environ.get("STACK_NAME")
-
-if 'prod' in stack_name:
-    stage = 'PROD'
-
-elif 'homolog' in stack_name:
-    stage = 'HOMOLOG'
-
-elif 'dev' in stack_name:
-    stage = 'DEV'
-
-else:
-    stage = 'TEST'
+stage = os.environ.get("GITHUB_REF_NAME").capitalize()
 
 tags = {
-    'project': 'Template',
+    'project': 'PortfolioTeiaCriativaApi',
     'stage': stage,
-    'stack': 'BACK',
+    'stack': stack_name,
     'owner': 'DevCommunity'
 }
 
-TemplateStack(app, stack_name=stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
+IacStack(app, 
+        stack_id=stack_name,
+        stack_name=stack_name,
+        env=cdk.Environment(account=aws_account_id, region=aws_region),
+        stage=stage,
+        tags=tags)
 
 
 app.synth()

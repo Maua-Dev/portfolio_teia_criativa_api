a3860fa8c31028025e100a57d903729e0fe960ea
pjdev12 <pjuan.devmaua@gmail.com>
2026-07-28 20:35:01 -0300

    Adaptação do controle, caso e viewmodel do novo modelo de usuário

--- src/modules/create_user/app/create_user_usecase.py
+++ src/modules/create_user/app/create_user_usecase.py
@@ -1,7 +1,5 @@
-
-
 from src.shared.domain.entities.user import User
-from src.shared.domain.enums.state_enum import STATE
+from src.shared.domain.enums.role_enum import RoleEnum
 from src.shared.domain.repositories.user_repository_interface import IUserRepository
 from src.shared.helpers.errors.domain_errors import EntityError
 
@@ -10,18 +8,14 @@ class CreateUserUsecase:
     def __init__(self, repo: IUserRepository):
         self.repo = repo
 
-    def __call__(self, name: str, email: str) -> User:
-
-        if not User.validate_name(name):
-            raise EntityError("name")
-
-        if not User.validate_email(email):
-            raise EntityError("email")
-
-        user = User(
-            name=name,
-            email=email,
-            state=STATE.PENDING
-        )
-
+    def __call__(self, email: str, senha_hash: str, role: RoleEnum = RoleEnum.USER) -> User:
+        try:
+            user = User(
+                email=email,
+                senha_hash=senha_hash,
+                role=role,
+            )
+        except Exception:
+            raise EntityError('User')
+       
         return self.repo.create_user(user)

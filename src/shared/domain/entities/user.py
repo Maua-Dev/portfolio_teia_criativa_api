68d714f7266ca371a2f916dc760dda0a2d48fe1d
pjdev12 <pjuan.devmaua@gmail.com>
2026-07-28 13:35:09 -0300

    Adição dos fields na entidade user

--- src/shared/domain/entities/user.py
+++ src/shared/domain/entities/user.py
@@ -1,13 +1,26 @@
 from enum import Enum
 from uuid import UUID, uuid4
+import uuid
 from pydantic import BaseModel, EmailStr, Field
 from src.shared.domain.enums.role_enum import RoleEnum
 
 class User(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
-    email: EmailStr
-    role: RoleEnum
-    senha_hash: str
+    id: uuid.UUID = Field(
+        default_factory=uuid.uuid4,
+        description="Id unico do usuário"
+    )
+    email: EmailStr = Field(
+        ...,
+        description="Email do usuário"
+    )
+    role: RoleEnum = Field(
+        default=RoleEnum.USER,
+        description="Role do usuário"
+    )
+    senha_hash: str = Field(
+        ...,
+        description="Hash da senha do usuário"
+    )
 
     class Config:
         frozen = True

70745ec5d751774a6a789c5e1df6a676fd442d63
Leo Iorio <lseixas.iorio@gmail.com>
2026-08-15 11:15:06 -0300

    fix: adjusted wtp test for new message

--- tests/modules/get_user/app/test_get_user_controller.py
+++ tests/modules/get_user/app/test_get_user_controller.py
@@ -50,7 +50,7 @@ class Test_GetUserController:
         response = controller(request=request)
 
         assert response.status_code == 400
-        assert response.body == "Field user_id isn't in the right type.\n Received: int.\n Expected: str"
+        assert response.body == "The field 'user_id' has the wrong type. Received: 'int'. Expected: 'str'."
 
     def test_get_user_contoller_entity_error(self):
         repo = UserRepositoryMock()

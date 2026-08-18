3e8fde61cb11e171e7d075c609d7143d880dd5d4
Gustavo Donato <gugaodonato@gmail.com>
2026-08-17 22:32:01 -0300

    feat: adiciona testes da rota delete_project

--- src/modules/delete_project/app/delete_project_viewmodel.py
+++ src/modules/delete_project/app/delete_project_viewmodel.py
@@ -6,16 +6,22 @@ class DeleteProjectViewmodel:
     id: uuid.UUID
     title: str
     description: str
+    associates: list[uuid.UUID]
+    display_image: str
 
     def __init__(self, project: Project):
         self.id = project.id
         self.title = project.title
         self.description = project.description
+        self.associates = project.associates
+        self.display_image = project.display_image
 
     def to_dict(self):
         return {
             'project_id': str(self.id),
             'title': self.title,
             'description': self.description,
+            'associates': self.associates,
+            'display_image': self.display_image,
             'message': "the project was deleted successfully"
         }

2e969a0c2ee1a99143baaf6cad5b0e764fa20dc4
Thiago <thiagosm2006@gmail.com>
2026-08-18 23:52:27 -0300

    feat: add module route update_user

--- src/modules/update_user/app/update_user_usecase.py
+++ src/modules/update_user/app/update_user_usecase.py
@@ -2,19 +2,22 @@ from src.shared.domain.entities.user import User
 from src.shared.domain.repositories.user_repository_interface import IUserRepository
 from src.shared.helpers.errors.domain_errors import EntityError
 
+import uuid
 
 class UpdateUserUsecase:
     def __init__(self, repo: IUserRepository):
         self.repo = repo
 
-    def __call__(self, user_id: int, new_name: str) -> User:
+    def __call__(self, user_id: str, new_email: str) -> User:
 
-        if type(user_id) != int:
+        try:
+            uuid.UUID(user_id)
+        except ValueError:
             raise EntityError("user_id")
         
-        if type(new_name) != str:
-            raise EntityError("new_name")
+        if type(new_email) != str:
+            raise EntityError("new_email")
 
-        updated_user = self.repo.update_user(user_id=user_id, new_name=new_name)
+        updated_user = self.repo.update_user(user_id=user_id, new_email=new_email)
 
         return updated_user

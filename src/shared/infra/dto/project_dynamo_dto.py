dec72adbdf56465e98a0f013876621a7d0865634
Gustavo Donato <gugaodonato@gmail.com>
2026-08-25 20:06:25 -0300

    feat: adiciona project_dynamo_dto

---
+++ src/shared/infra/dto/project_dynamo_dto.py
@@ -0,0 +1,75 @@
+from src.shared.domain.entities.project import Project
+import uuid
+
+
+class ProjectDynamoDTO:
+    title: str
+    description: str
+    associates: list[uuid.UUID]
+    display_image: str
+    project_id: uuid.UUID
+
+    def __init__(self, title: str, description: str, associates: list[uuid.UUID], display_image: str, project_id: uuid.UUID):
+        self.title = title
+        self.description = description
+        self.associates = associates
+        self.display_image = display_image
+        self.project_id = project_id
+
+    @staticmethod
+    def from_entity(project: Project) -> "ProjectDynamoDTO":
+        """
+        Parse data from Project to ProjectDynamoDTO
+        """
+        return ProjectDynamoDTO(
+            title=project.title,
+            description=project.description,
+            associates=project.associates,
+            display_image=project.display_image,
+            project_id=project.project_id
+        )
+
+    def to_dynamo(self) -> dict:
+        """
+        Parse data from ProjectDynamoDTO to dict
+        """
+        return {
+            "entity": "project",
+            "title": self.title,
+            "description": self.description,
+            "associates": [str(associate) for associate in self.associates],
+            "display_image": self.display_image,
+            "project_id": str(self.project_id),
+        }
+
+    @staticmethod
+    def from_dynamo(project_data: dict) -> "ProjectDynamoDTO":
+        """
+        Parse data from DynamoDB to ProjectDynamoDTO
+        @param project_data: dict from DynamoDB
+        """
+        return ProjectDynamoDTO(
+            title=project_data["title"],
+            description=project_data["description"],
+            associates=[uuid.UUID(associate) for associate in project_data["associates"]],
+            display_image=project_data["display_image"],
+            project_id=uuid.UUID(project_data["project_id"])
+        )
+
+    def to_entity(self) -> Project:
+        """
+        Parse data from ProjectDynamoDTO to Project
+        """
+        return Project(
+            title=self.title,
+            description=self.description,
+            associates=self.associates,
+            display_image=self.display_image,
+            project_id=self.project_id
+        )
+
+    def __repr__(self):
+        return f"ProjectDynamoDto(title={self.title}, description={self.description}, associates={self.associates}, display_image={self.display_image}, project_id={self.project_id})"
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
